f1e1463b51ed9db9ba5d50c47edb4b6c233e2010
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-02-13 21:09:47 +0500

    Exported different components constants

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const navbarItems = [
+   {
+      id: 1,
+      title: "Services",
+      href: "services",
+   },
+   {
+      id: 2,
+      title: "Our work",
+      href: "our work",
+   },
+   {
+      id: 3,
+      title: "About us",
+      href: "about us",
+   },
+   {
+      id: 4,
+      title: "Insights",
+      href: "insights",
+   },
+   {
+      id: 5,
+      title: "Contact us",
+      href: "contact us",
+   },
+];
