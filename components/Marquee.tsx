f83199f741d551270a7eee579fc6ce61136899bb
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-01 10:16:39 +0500

    Completed

---
+++ components/Marquee.tsx
@@ -0,0 +1,18 @@
+import { TextMarquee } from "@/animation";
+
+export default function Marquee() {
+	return (
+		<>
+			<div className="w-full bg-marquee rounded-t-[20px] py-[80rem] ">
+				<TextMarquee baseVelocity="1.5">
+					<h1 className="text-[500rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
+						we are ochi &nbsp;
+					</h1>
+					<h1 className="text-[500rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
+						we are ochi &nbsp;
+					</h1>
+				</TextMarquee>
+			</div>
+		</>
+	);
+}
