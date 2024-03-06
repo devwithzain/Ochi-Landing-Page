85fb668438e86dbabd0c1b77b786a4e2c55a5b07
Muhammad Zain <ZainSoftwear11@gmail.com>
2024-03-06 12:12:12 +0500

    Modified

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -4,11 +4,11 @@ export default function Marquee() {
 	return (
 		<>
 			<div className="w-full bg-marquee rounded-t-[20px] py-[80rem] ">
-				<TextMarquee baseVelocity="1.5">
-					<h1 className="text-[500rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
+				<TextMarquee baseVelocity="1">
+					<h1 className="text-[480rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
 						we are ochi &nbsp;
 					</h1>
-					<h1 className="text-[500rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
+					<h1 className="text-[480rem] font-FoundersGrotesk font-normal border-y border-gray-100 pb-[50px] uppercase text-white leading-[380rem] p-0 whitespace-nowrap tracking-[-5px]">
 						we are ochi &nbsp;
 					</h1>
 				</TextMarquee>
